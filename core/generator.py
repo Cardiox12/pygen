4e7eb2c4f681c944f30f066787afc40daadcf419
Tony Bellavia <at.bellavia@gmail.com>
2019-10-05 16:35:40 +0200

    On probability chose.
    Should improve the capability to chose multiple word in a set of
    words which have the best probability to be the next word.

--- core/generator.py
+++ core/generator.py
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    generator.py                                       :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: tony <tony@student.42.fr>                  +#+  +:+       +#+         #
+#    By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/01 15:58:58 by bbellavi          #+#    #+#              #
-#    Updated: 2019/10/01 22:45:58 by tony             ###   ########.fr        #
+#    Updated: 2019/10/05 16:34:50 by bbellavi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -71,7 +71,11 @@ class Generator(object):
 
             for k, v in value.items():
                 probs[k] = v / total
-            tokens_probs[key] = probs
+                
+            tokens_probs[key] = {
+                "probs": probs, 
+                "total" : total
+            }
             probs = {}
         
         self.probabilities = tokens_probs
@@ -94,29 +98,33 @@ class Generator(object):
             first_token = first_token[0]
 
         next_token = first_token
+        
         while True:
             yield next_token
 
-            next_token = sorted(self.probabilities[next_token].items(), key=lambda x: x[1])
+            probs = self.probabilities[next_token]['probs']
+            limit = 1 / self.probabilities[next_token]['total']
+            next_token = sorted(probs.items(), key=lambda x: x[1])
+            
             if len(next_token) > 3:
                 next_token = next_token[randint(0, len(next_token) - 1)][0]
             else:
                 next_token = next_token[0][0]
+
+
+    def get_n_next_word(self, n):
+        generator = self.get_next_word()
+        generated = []
+
+        for _ in range(n):
+            generated.append(generator.__next__())
+
+        return generated
 
 if __name__ == "__main__":
     with open("file_test/obama.txt") as f:
         data = f.read()
         gen = Generator(data)
         gen.train()
-        
-        for i in range(10):
-            generator = gen.get_next_word()
-            sentence = []
-
-            for _ in range(20):
-                sentence.append(generator.__next__())
-
-            print(f"========================================== PHRASE {i} ==========================================")
-            print() 
-            print(" ".join(sentence))
-            print() 
+        liste = gen.get_n_next_word(5)
+        print(liste)
